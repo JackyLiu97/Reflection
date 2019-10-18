c81e5dae0759763c70b1d1eece8f97f398b11423
william <willchung42@gmail.com>
2019-10-18 14:36:56 -0400

    fixed an issue with bot not reading prefix correctly

--- reflection.js
+++ reflection.js
@@ -1,15 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
+const config = require('./config.json');
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-	if (msg.content.toLowerCase() === 'ping') {
-		msg.reply('Pong!');
+client.on('message', message => {
+	
+	if (message.content.startsWith(config.prefix + 'ping')) {
+		message.channel.send('Pong!');
+		console.log(config.prefix);
 	}
+
+if (message.content.startsWith(config.prefix + 'delete')) {
+    message.delete(1000);
+	message.reply('Deleting messages!').then(deleted_msg => {deleted_msg.delete(1000); });
+    
+}
 });
+
 
 client.login(auth.token);

bc28438db6a98ca3a5725085b1e222d7d25b39fd
william <willchung42@gmail.com>
2019-10-18 13:54:10 -0400

    up to date version

--- reflection.js
+++ reflection.js
@@ -3,7 +3,13 @@ const client = new Discord.Client();
 const auth = require('./auth.json');
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+	if (msg.content.toLowerCase() === 'ping') {
+		msg.reply('Pong!');
+	}
 });
 
 client.login(auth.token);

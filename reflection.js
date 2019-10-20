4de8371e64143b99cba98b1b173fad92a9ee0404
JackyLiu97 <jackyliu977@gmail.com>
2019-10-20 00:20:12 -0400

    dev branch test

--- reflection.js
+++ reflection.js
@@ -35,10 +35,47 @@ client.on('message', message => {
 		let numToDelete = parseInt(args[0], 10) + 1;
 		
 		async function clear() {
-			let fetched = await message.channel.fetchMessages({limit:numToDelete});
+			let fetched = await message.channel.fetchMessages({limit:numToDelete}); // this is a collection , map
+			let fetched_messages = fetched.array()
+			let toDelete = []
+			for (let i = 0; i < fetched_messages.length; i++) {
+				console.log(fetch_messages[i].attachments + 'has attachments!')
+				if (fetched_messages[i].attachments.size <= 0 ) { // if the message doesnt have any attachments
+					toDelete.push(fetched_messages[i])
+				}
+				else {
+					if (fetched_messages[i].attachments.size > 0 ) {
+						let url = fetched_messages[i].attachments.url
+						let format_check = url.indexOf("png", url.length - "png".length) || url.indexOf("jpg", url.length - "jpg".length)
+						if (format_check == -1 || typeof(format_check) === 'Undefined') {
+						toDelete.push(fetch_messages[i])
+							}
+				}	
+			}	
+		}
+		toDelete.forEach(message => message.delete())
+	}
+
+			/* 
+			msg_arr.filter(msg => !msg.attachments.size > 0) //filters out all the messages that has no images
+			for (let i = 0; i < msg_arr.length; i++) {
+				if (msg_arr[i].attachments.message) { // we know the array contains an image, but check if it contains a message
+					let url = msg_arr[i].attachments.url
+					let url_bool = url.indexOf("png", url.length - "png".length) || url.indexOf("jpg", url.length - "jpg".length)
+					if (url_bool != -1 ) {
+						img_arr.push(url)
+					}
+				}	
+			}
+			*/	
+			/*
+			if (message.channel.attachments.message) {
+				let url = message.channel.attachments.url
+				console.log("Hello world")
+			}
 			message.channel.bulkDelete(fetched);	
 		}
-
+		*/
 		if (Number.isInteger(numToDelete)) {
 			console.log((numToDelete - 1) + ' messages deleted!');
 			clear();
@@ -46,7 +83,7 @@ client.on('message', message => {
 		
 	}
 
-	// deletes based on time // not done bot crashes when I run it lol
+	// deletes based on time 
 	if (command === 'deletetime') {
 		let minutes = parseInt(args[0],10) * 60 // minutes expressed in seconds 
 		async function clear() {
@@ -54,7 +91,7 @@ client.on('message', message => {
 			let mess_arr = messages.array()
 			let TimeStamp = Math.floor(Date.now() / 1000)  //TimeStamp expressed in seconds
 			// Date.now() returns the current timestamp expressed in milliseconds. 
-			let filter = TimeStamp - minutes  //minimum threshold ?
+			let filter = TimeStamp - minutes  // cannot exceed maxiumum threshold 
 			//console.log("Current Timestamp is " + TimeStamp)
 			//console.log("current filter is "+ filter)
 			let filter_msg = []
